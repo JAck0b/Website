f0ded647b941e4442841f3252c9cefdf55b692c3
jakub <filistynskikuba@gmail.com>
2018-11-10 21:39:17 +0100

    New name

--- skrypt.sh
+++ skrypt.sh
@@ -3,6 +3,6 @@ while [ true ];do
   links -dump $1 > ./website.txt
   echo "Difference: "
   diff ./website.txt ./site.txt
-  cat ./website > ./site.txt
-  git commit ./site.txt -m "New website."
+  cat ./website.txt > ./site.txt
+  git commit ./site.txt -m "New site."
 done
