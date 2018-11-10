5981a754a113407345bc1e27de65728196c74a0b
jakub <filistynskikuba@gmail.com>
2018-11-10 23:05:26 +0100

    new Script

--- skrypt.sh
+++ skrypt.sh
@@ -1,3 +1,6 @@
+links -dump $1 > ./website.txt
+cat ./website.txt > ./site.txt
+git commit ./site.txt ./website.txt -m "New site." > /dev/null
 while [ true ];do
   sleep $2
   links -dump $1 > ./website.txt
